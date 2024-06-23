ee4e38d2245d5d51813d82c2940b667efe9bcb7c
Erman <ermanw@Omars-MacBook-Pro.local>
2024-06-23 17:56:38 +0100

    j'ai encore rien changer c'le C_1

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Students;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Students::all();
+
+        return view('index', compact('students'));
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('create');
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData=$request->validate([
+            'name'=> 'required | max: 255',
+            'email'=>'required | max: 255',
+            'phone'=>'required | max: 255',
+            'section'=>'required | max: 255',
+            'image'=>'required | image|mimes:jpg,jpeg,png,gif,svg',
+        ]);
+        $image = $request->file('image');
+        $destinationPath = 'image/';
+        $profileImage=date('YmdHis').".".$image->getClientOriginalExtension();
+        $image->move($destinationPath, $profileImage);
+        $validateData['image']= $profileImage;
+        
+        $students = Students::create($validateData);
+
+        return redirect('/students');
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+<h3> Students List </h3>
+
+@endsection()
